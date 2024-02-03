ea66ad65777b184975053159ed5fe7aee95aed09
Akinwale Habib <akinwalehabib@hotmail.com>
2024-02-03 17:27:49 +0100

    edit config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://akinwale-habibullah.github.io',
+  url: 'https://crashcours.es',
+  // 'https://akinwale-habibullah.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/crashcourses/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
